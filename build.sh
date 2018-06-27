26175c3b5dfe799292977d297648344c9669d2a0
Rafay Khan <rafay@autonomic.ai>
2018-06-27 16:09:21 -0400

    Deploy instead of install

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 ./mvnw versions:set -DnewVersion=$FINAL_OR_BRANCH_SNAPSHOT_VERSION
-./mvnw install -f ./spring-cloud-kubernetes-dependencies/
+./mvnw deploy -f ./spring-cloud-kubernetes-dependencies/
 ./mvnw deploy
